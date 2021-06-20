fc00c9dc2eeb68fba0f839b8c77808df1496d9ed
kaku <scugjs@gmail.com>
2021-06-20 08:51:43 +0800

    [add] abstract factory

---
+++ src/main/java/com/github/kakusosaku/designpattern/creational/factory/abstracted/Summary.java
@@ -0,0 +1,15 @@
+package com.github.kakusosaku.designpattern.creational.factory.abstracted;
+
+/**
+ * Description
+ *
+ * @author kaku
+ * Date    6/15/21
+ */
+public class Summary {
+
+    public static void main(String[] args) {
+
+    }
+   
+}
